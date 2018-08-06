bdb4c04d5c98844d14044377cd19c9e71efa2805
sidfubo1 <sidfubo1@gmail.com>
2018-08-06 23:50:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function renderBooks(json) {
 }
 
 function renderHouses(json) {
-  $("#div-houses").html(json.name);
+  $("#div-houses p").html(json.name);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
