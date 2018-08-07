30ce35b6c4b26ae23da6724185850aacd336e114
sidfubo1 <sidfubo1@gmail.com>
2018-08-07 00:02:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 function fetchBooks() {
   //write fetch request to the Game of Thrones API
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(renderBooks)
 }
 
+// function fetchHouses() {
+//   fetch('https://anapioficeandfire.com/api/houses')
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(renderHouses)
+// }
+
 function renderBooks(json) {
+  debugger;
   const main = document.querySelector('main')
   json.forEach(book => {
     const h2 = document.createElement('h2')
@@ -10,6 +24,10 @@ function renderBooks(json) {
     main.appendChild(h2)
   })
 }
+
+// function renderHouses(json) {
+//   $("#div-houses p").append(json.name);
+// }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
